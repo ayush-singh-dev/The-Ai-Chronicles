82d431b5dc15ed3c7b93bee04b7795cc63156c9d
Ayush Singh <ayush777rawat@gmail.com>
2024-12-26 12:41:49 +0530

    update mongodb

--- backend/configs/mongoDb.js
+++ backend/configs/mongoDb.js
@@ -4,10 +4,16 @@ const URI = process.env.MONGODB_URI;
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(URI);
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 30000, // Increase connection timeout to 30 seconds
+      socketTimeoutMS: 30000, // Increase socket timeout to 30 seconds
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("connection failed", error.message);
+    process.exit(1);
   }
 };
 
